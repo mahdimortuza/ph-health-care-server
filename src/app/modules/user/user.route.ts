850bd51411b8e31210959f692d6c625f8ab063ae
mahdimortuza <mahdi.webx@gmail.com>
2024-03-31 22:20:20 +0600

    done

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,33 +1,38 @@
 import express, { NextFunction, Request, Response } from 'express';
-import { UserController } from './user.controller';
-import validateRequest from '../../middlewares/validateRequest';
-import { UserValidation } from './user.validations';
-import auth from '../../middlewares/auth';
 import { ENUM_USER_ROLE } from '../../../enums/user';
 import { FileUploadHelper } from '../../../helpers/fileUploadHelper';
+import auth from '../../middlewares/auth';
+import validateRequest from '../../middlewares/validateRequest';
+import { UserController } from './user.controller';
+import { UserValidation } from './user.validations';
 
 const router = express.Router();
 
 router.get(
   '/',
   auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
-  UserController.getAllUser
+  UserController.getAllUser,
 );
 
 router.get(
   '/me',
-  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.DOCTOR, ENUM_USER_ROLE.PATIENT, ENUM_USER_ROLE.SUPER_ADMIN),
-  UserController.getMyProfile
-)
+  auth(
+    ENUM_USER_ROLE.ADMIN,
+    ENUM_USER_ROLE.DOCTOR,
+    ENUM_USER_ROLE.PATIENT,
+    ENUM_USER_ROLE.SUPER_ADMIN,
+  ),
+  UserController.getMyProfile,
+);
 
 router.post(
   '/create-doctor',
-  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  // auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   FileUploadHelper.upload.single('file'),
   (req: Request, res: Response, next: NextFunction) => {
-    req.body = UserValidation.createDoctor.parse(JSON.parse(req.body.data))
-    return UserController.createDoctor(req, res, next)
-  }
+    req.body = UserValidation.createDoctor.parse(JSON.parse(req.body.data));
+    return UserController.createDoctor(req, res, next);
+  },
 );
 
 router.post(
@@ -35,18 +40,18 @@ router.post(
   auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   FileUploadHelper.upload.single('file'),
   (req: Request, res: Response, next: NextFunction) => {
-    req.body = UserValidation.createAdmin.parse(JSON.parse(req.body.data))
-    return UserController.createAdmin(req, res, next)
-  }
+    req.body = UserValidation.createAdmin.parse(JSON.parse(req.body.data));
+    return UserController.createAdmin(req, res, next);
+  },
 );
 
 router.post(
   '/create-patient',
   FileUploadHelper.upload.single('file'),
   (req: Request, res: Response, next: NextFunction) => {
-    req.body = UserValidation.createPatient.parse(JSON.parse(req.body.data))
-    return UserController.createPatient(req, res, next)
-  }
+    req.body = UserValidation.createPatient.parse(JSON.parse(req.body.data));
+    return UserController.createPatient(req, res, next);
+  },
 );
 
 router.patch(
@@ -58,12 +63,17 @@ router.patch(
 
 router.patch(
   '/update-my-profile',
-  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.DOCTOR, ENUM_USER_ROLE.PATIENT),
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+    ENUM_USER_ROLE.DOCTOR,
+    ENUM_USER_ROLE.PATIENT,
+  ),
   FileUploadHelper.upload.single('file'),
   (req: Request, res: Response, next: NextFunction) => {
-    req.body = JSON.parse(req.body.data)
-    return UserController.updateMyProfile(req, res, next)
-  }
+    req.body = JSON.parse(req.body.data);
+    return UserController.updateMyProfile(req, res, next);
+  },
 );
 
 export const userRoutes = router;
